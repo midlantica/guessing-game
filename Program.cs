2288fa092856a059d89209f435e682bc2047dedf
Drew Harper <druharper@gmail.com>
2020-08-25 10:34:41 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you guess the secret number?");
+            string numberGuess = Console.ReadLine();
+            Console.WriteLine($"Your guess was... {numberGuess}");
+        }
+    }
+}
